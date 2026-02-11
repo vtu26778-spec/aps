f7007fbd2185a31684ad1480eb07157445ab5a63
vtu26778-spec <vtu26778@veltech.edu.in>
2026-02-11 09:18:03 +0530

    Time: 4 ms (82.53%), Space: 85.7 MB (49.94%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
@@ -1,33 +1,25 @@
 class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
-        int len = nums.length;
-        int[] diff = new int[len-1];
-        for(int i = 0;i < len-1;i++){
-            diff[i] = nums[i+1]-nums[i];
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
         }
 
-        int[] forward = new int[len-1];
-        int[] backward = new int[len-1];
-        for(int i = 0;i < len-1;i++){
-            forward[i] = diff[i]*((len-i)-1);
-            backward[i] = diff[i]*(i+1);
+        int prefixSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int left = nums[i] * i - prefixSum;
+            int right = (totalSum - prefixSum - nums[i])
+                        - nums[i] * (n - i - 1);
+
+            result[i] = left + right;
+            prefixSum += nums[i];
         }
 
-        for(int i = 1;i < len-1;i++){
-            forward[i] += forward[i-1];
-            backward[i] += backward[i-1];
-        }
-
-        int[] ans = new int[len];
-        ans[0] = forward[len-2];
-        ans[len-1] = backward[len-2];
-
-        for(int i = 1;i < len-1;i++){
-            int diff1 = forward[len-2] - forward[i-1];
-            int diff2 = backward[i-1];
-            ans[i] = diff1+diff2;
-        }
-        
-        return ans;
+        return result;
     }
 }
+
